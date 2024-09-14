5953e027975aa309c186de9e8aa2fd7e2da4faee
Alief Twumasi <alieftwu@gmail.com>
2024-09-14 00:41:06 -0500

    edited environment, added animations

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public Transform world;
+    public float rotationSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        world = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotates the planet
+        world.Rotate(new Vector3(0,rotationSpeed * Time.deltaTime, 0), Space.World);
+    }
+}
